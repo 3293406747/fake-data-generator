c16e3dc46345f0cb7a61cb0158eb003efd800709
py85329140 <3293406747@qq.com>
2023-05-21 15:27:26 +0800

    last commit

--- web/static/sources.js
+++ web/static/sources.js
@@ -1,21 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("create").addEventListener('click', function() {
+  document.getElementById("create").addEventListener('click', async function() {
     var mySelect = document.getElementById("choose");
-    var index = mySelect.selectedIndex
-    var text = mySelect.options[index].text
+    var index = mySelect.selectedIndex;
+    var text = mySelect.options[index].text;
     var list = document.getElementById("list");
     var span = document.createElement("span");
+
     var url = 'http://127.0.0.1:5000/data?target=' + encodeURI(mySelect.value);
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        span.innerText = text+": "+data["value"]+"\n";
-        list.appendChild(span);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      var response = await fetch(url);
+      var data =  await response.json();
+    } catch (error) {
+      console.log(error)
+    };
+
+    span.innerText = text+": "+data["value"]+"\n";
+    list.appendChild(span);
+
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     span.innerText = text+": "+data["value"]+"\n";
+    //     list.appendChild(span);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   });
+
   document.getElementById("clear").addEventListener('click', function() {
     var list = document.getElementById("list");
     list.remove();
@@ -23,6 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     new_list["id"] = "list";
     document.querySelector("body").appendChild(new_list);
     var mySelect = document.getElementById("choose");
+    
     mySelect.selectedIndex = 0;
   });
 });
